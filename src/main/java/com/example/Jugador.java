3d3e768f820cd4474ca1e51e53783b3d0d70beee
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-24 13:44:46 +0100

    Creacion de las clases: jugador y Presidente; scon sus respectivos getters and setters y sus constructores

--- src/main/java/com/example/Jugador.java
+++ src/main/java/com/example/Jugador.java
@@ -1,6 +1,65 @@
 package com.example;
 
 class Jugador {
-    system.out.println("hola");
+    public String Nommbre_camiseta;
+    public Date Fecha_nacimientio;
+    public String Pais_origen;
+    public Enum Posicion{Portero, Defensa, Medio_Campista, Delantero, Aguatero};
+    public int dorsal;
+    public Enum Traspaso{SIN_SOLICITAR,SOLICITADO,
+                APROBADO_POR_ENTRENADOR,
+                APROBADO_POR_PRESIDENTE,
+                RECHAZADO_POR_ENTRENADOR,
+                RECHAZADO_POR_PRESIDENTE};
+
+    public Jugador(String nommbre_camiseta, Date fecha_nacimientio, String pais_origen, Enum  , int dorsal,
+            Enum traspaso) {
+        Nommbre_camiseta = nommbre_camiseta;
+        Fecha_nacimientio = fecha_nacimientio;
+        Pais_origen = pais_origen;
+        Posicion = posicion;
+        this.dorsal = dorsal;
+        Traspaso = traspaso;
+    }
+
+    public String getNommbre_camiseta() {
+        return Nommbre_camiseta;
+    }
+
+    public void setNommbre_camiseta(String nommbre_camiseta) {
+        Nommbre_camiseta = nommbre_camiseta;
+    }
+
+    public Date getFecha_nacimientio() {
+        return Fecha_nacimientio;
+    }
+
+    public void setFecha_nacimientio(Date fecha_nacimientio) {
+        Fecha_nacimientio = fecha_nacimientio;
+    }
+
+    public String getPais_origen() {
+        return Pais_origen;
+    }
+
+    public void setPais_origen(String pais_origen) {
+        Pais_origen = pais_origen;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public Enum getTraspaso() {
+        return Traspaso;
+    }
+
+    public void setTraspaso(Enum traspaso) {
+        Traspaso = traspaso;
+    };
 
 }
---
+++ src/main/java/com/example/Presidente.java
@@ -0,0 +1,27 @@
+package main.java.com.example;
+
+public class Presidente {
+    public String Dni;
+    public String Nombre;
+
+    public Presidente(String dni, String nombre) {
+        Dni = dni;
+        Nombre = nombre;
+    }
+
+    public String getDni() {
+        return Dni;
+    }
+
+    public void setDni(String dni) {
+        Dni = dni;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+}
