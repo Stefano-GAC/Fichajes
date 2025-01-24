da6cf5bd74a9436ec54f91df75700214ebdc9e16
DESKTOP-L7H1FM4\Usuario <Camila-GitHub>
2025-01-24 13:44:32 +0100

    Creación clases Entrenador y Equipo con sus "Constructores", "Getters" y "Setters".

---
+++ src/main/java/com/example/Entrenador.java
@@ -0,0 +1,25 @@
+package com.example;
+
+class Entrenador {
+    public String nombreEntrenador;
+
+    public enum formacion {
+        Formacion532, Formacion451, Formacion442, Formacion433, Formacion343, Formacion424, Formacion4141, Formacion352
+    };
+
+    /**
+     * Constructor de Jugador
+     * @param nombreEntrenador
+     */
+    public Entrenador(String nombreEntrenador) {
+        this.nombreEntrenador = nombreEntrenador;
+    }
+
+    public String getNombreEntrenador() {
+        return nombreEntrenador;
+    }
+
+    public void setNombreEntrenador(String nombreEntrenador) {
+        this.nombreEntrenador = nombreEntrenador;
+    }
+}
