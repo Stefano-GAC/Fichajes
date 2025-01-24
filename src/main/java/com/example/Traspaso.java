c9c980e9368d92981efa5a4cf50f20ebcd604eed
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-24 14:10:13 +0100

    Creado enum Traspaso.

---
+++ src/main/java/com/example/Traspaso.java
@@ -0,0 +1,9 @@
+package main.java.com.example;
+
+public enum Traspaso {
+    SIN_SOLICITAR, SOLICITADO,
+    APROBADO_POR_ENTRENADOR,
+    APROBADO_POR_PRESIDENTE,
+    RECHAZADO_POR_ENTRENADOR,
+    RECHAZADO_POR_PRESIDENTE;
+}
