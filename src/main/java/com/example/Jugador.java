3f1c8b822ae717beaf3bbc45ee2994ca077d77ac
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-24 14:16:58 +0100

    Jugador arreglado.

--- src/main/java/com/example/Jugador.java
+++ src/main/java/com/example/Jugador.java
@@ -1,26 +1,23 @@
-package com.example;
+package main.java.com.example;
 
 class Jugador {
     public String Nommbre_camiseta;
     public Date Fecha_nacimientio;
     public String Pais_origen;
-    public Enum Posicion{Portero, Defensa, Medio_Campista, Delantero, Aguatero};
+    public Posicion p;
     public int dorsal;
-    public Enum Traspaso{SIN_SOLICITAR,SOLICITADO,
-                APROBADO_POR_ENTRENADOR,
-                APROBADO_POR_PRESIDENTE,
-                RECHAZADO_POR_ENTRENADOR,
-                RECHAZADO_POR_PRESIDENTE};
+    public Traspaso t;
 
-    public Jugador(String nommbre_camiseta, Date fecha_nacimientio, String pais_origen, Enum  , int dorsal,
-            Enum traspaso) {
-        Nommbre_camiseta = nommbre_camiseta;
-        Fecha_nacimientio = fecha_nacimientio;
-        Pais_origen = pais_origen;
-        Posicion = posicion;
-        this.dorsal = dorsal;
-        Traspaso = traspaso;
-    }
+    // public Jugador(String nommbre_camiseta, Date fecha_nacimientio, String
+    // pais_origen, Enum , int dorsal,
+    // Enum traspaso) {
+    // Nommbre_camiseta = nommbre_camiseta;
+    // Fecha_nacimientio = fecha_nacimientio;
+    // Pais_origen = pais_origen;
+    // Posicion = posicion;
+    // this.dorsal = dorsal;
+    // Traspaso = traspaso;
+    // }
 
     public String getNommbre_camiseta() {
         return Nommbre_camiseta;
