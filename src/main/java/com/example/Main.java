b0f244d480120985e7986e47fed8fba92499644d
sac0024 <sac0024@alu.medac.es>
2025-01-25 18:01:34 -0500

    Creacion de objeto de la clase jugador

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,11 +8,18 @@ public class Main {
         Presidente presidente2 = new Presidente("123456789B", "Camila Gonzalez");
         Presidente presidente3 = new Presidente("123456789C", "Jose Rpdriguez");
     }
-    /**Creacion de la lista de presidentes */
-    ArrayList<Presidente> presidentes = new ArrayList<>();
-    presidentes.add(presidente1);
-    presidentes.add(presidente2);
-    presidentes.add(presidente3);
+        /**Creacion de la lista de presidentes */
+        ArrayList<Presidente> presidentes = new ArrayList<>();
+        presidentes.add(presidente1);
+        presidentes.add(presidente2);
+        presidentes.add(presidente3);
 
+        /**Creacion de objeto de la clase jugador */
+        Posicion posicionPortero = new Posicion.Portero;
+        Posicion posicionDefensa = new Posicion.Defensa;
+        Posicion posicionMedio_Campista = new Posiciocion.Medio_Campista;
+        Posicion posicionDelantero = new Posicion.Delantero;
+
+        
 
 }
