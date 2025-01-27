8e87dd3ffbc1dc1e14bea3ff93d4d10e1b46080f
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-27 14:07:57 +0100

    Agregando mas jugadores al array

--- src/main/java/com/example/Entrenador.java
+++ src/main/java/com/example/Entrenador.java
@@ -1,23 +1,27 @@
 package com.example;
 
 public class Entrenador {
-    private String nombreEntrenador;
-    private Formacion formacionPreferida; 
+    public String nombreEntrenador;
+    public Formacion formacionPreferida;
 
     // Constructor
     public Entrenador(String nombreEntrenador, Formacion formacionPreferida) {
-        this.nombreEntrenador = nombreEntrenador;
-        this.formacionPreferida = formacionPreferida;
+        nombreEntrenador = nombreEntrenador;
+        formacionPreferida = formacionPreferida;
     }
+
     public String getNombreEntrenador() {
         return nombreEntrenador;
     }
+
     public void setNombreEntrenador(String nombreEntrenador) {
         this.nombreEntrenador = nombreEntrenador;
     }
+
     public Formacion getFormacionPreferida() {
         return formacionPreferida;
     }
+
     public void setFormacionPreferida(Formacion formacionPreferida) {
         this.formacionPreferida = formacionPreferida;
     }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,8 @@
 package com.example;
+
 import java.util.ArrayList;
 import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         /** Creacion de objetos de la clase Presidente */
@@ -32,13 +34,27 @@ public class Main {
         Jugador jugador1 = new Jugador("Luis Suárez", new Date(1987, 1, 24), "Uruguay", posicionDelantero, 9, traspasoAprobadoPresidente);
         Jugador jugador2 = new Jugador("Gerard Piqué", new Date(1987, 2, 2), "España", posicionDefensa, 3, traspasoSolicitado);
         Jugador jugador3 = new Jugador("Thibaut Courtois", new Date(1992, 5, 11), "Bélgica", posicionPortero, 1, traspasoRechazadoEntrenador);
+        Jugador jugador4 = new Jugador("Leonel Messi", new Date (1984, 5, 26),"Argentina" , posicionMedioCampista, 10, traspasoAprobadoPresidente);
+        Jugador jugador5 = new Jugador("Stefano Alderete", new Date(2000, 8, 21), "Peru", posicionAguatero, 0, traspasoRechazadoPresidente);
+        Jugador jugador6 = new Jugador("Vini Jr", new Date(2000,6,21), "Brasil", posicionDelantero, 9, traspasoSolicitado)
+        Jugador jugador7 = new Jugador("Van Dijk", new Date(1998, 6,11), "Paises Bajos", posicionDefensa, 6666, traspasoSinSolicitar)
+        Jugador jugador8 = new Jugador("Lamine Yamal", new Date(2006, 9,25), "España", posicionDelantero, 11, traspasoAprobadoEntrenador)
+        Jugador jugador9 = new Jugador("Andres Iniesta", new Date(1889, 7, 1), "España", posicionMedioCampista, 8, traspasoRechazadoPresidente)
+        Jugador jugador10 = new Jugador("Oblak", new Date(1990, 12,30), "Eslovenia", posicionPortero, 4, traspasoSinSolicitar)
 
         /**Creacion de la lista jugadores */
         ArrayList<Jugador> jugadores = new ArrayList<>();
         jugadores.add(jugador1);
         jugadores.add(jugador2);
-        jugadores.add(jugador3);
+        jugadores.add(jugador3) ;
+        jugadores.add(jugador4);
+        jugadores.add(jugador5);
+        jugadores.add(jugador6);
+        jugadores.add(jugador7);
+        jugadores.add(jugador8) ;
+        jugadores.add(jugador9);
+        jugadores.add(jugador10);
 
     }
 
-    }
+}
